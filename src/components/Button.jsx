8a049078fe7e6f991838a62d6f7f19bbc858ab5e
Jagadeeshwar213 <jagadeeshwarreddy213@gmail.com>
2025-04-30 02:45:43 +0000

    Main

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,7 @@
+@@ -1,5 +1,6 @@
 import React from 'react'
+
 
 export default function Button({text, handleClick}) {
   return (
-    <a href="#" className="f5 no-underline black bg-animate hover-bg-black hover-white inline-flex items-center pa3 ba border-box mr4" onClick={handleClick}>
-      <span className="pl1">{text}</span>
-    </a>
-  )
-}
+    <a href="#" className="f5 no-underline black bg-animate hover-bg-black hover-white inline-flex items-center pa3 ba border-box mr4" onClick={handleClick}></a>
